3fe9a127e16d00a6e66b31c26128ef86cae1bf02
Denis Huamán <denis07.1993@gmail.com>
2019-05-08 14:25:09 -0500

    Agregando bootstrap al proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   mensaje = 'Curso de Angular';
   nombre = 'denishuaman';
   imagenURL = 'http://lorempixel.com/400/200';
   estadoBoton = true;
   esActivo = true;
+  txtClassBotonActivo = '';
   persona = {
     nombre: 'Denis',
     edad: 25
   };
+
+  constructor() { }
+
+  ngOnInit() {
+    console.log('Metodo created');
+    this.txtClassBotonActivo = this.esActivo ? 'Botón con class' : 'Botón sin class';
+  }
 
   getNombre() {
     return this.nombre;
@@ -22,19 +30,23 @@ export class AppComponent {
 
   save(e) {
     console.log(e);
+    alert('Mouse event: ' + e.type);
   }
 
   onKeyUp(e) {
     if(e.keyCode === 13) {
       console.log('ENTER fue presionado');
+      alert('ENTER fue presionado');
     }
   }
 
   onKeyUpEnter() {
     console.log('ENTER fue presionado');
+    alert('ENTER fue presionado');
   }
 
   onKeyUp2(usuario) {
     console.log('Texto ingresado:', usuario);
+    alert('Texto ingresado: ' + usuario);
   }
 }
